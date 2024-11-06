339a9a1c942e5c8a643342610c4ba0e03f9f2447
reggidev <reggidev@gmail.com>
2024-11-06 13:33:26 -0300

    feat: add authentication with clerk

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,17 @@
+import { SignInButton } from '@clerk/nextjs'
+import { auth } from '@clerk/nextjs/server'
 import { LogInIcon } from 'lucide-react'
 import Image from 'next/image'
+import { redirect } from 'next/navigation'
 
 import { Button } from '../_components/ui/button'
 
-const LoginPage = () => {
+const LoginPage = async () => {
+  const { userId } = await auth()
+  if (userId) {
+    redirect('/')
+  }
+
   return (
     <div className="grid h-full grid-cols-2">
       {/* ESQUERDA */}
@@ -21,10 +29,12 @@ const LoginPage = () => {
           monitorar suas movimentações, e oferecer insights personalizados,
           facilitando o controle do seu orçamento
         </p>
-        <Button variant="outline">
-          <LogInIcon className="mr-2" />
-          Fazer login ou criar conta
-        </Button>
+        <SignInButton>
+          <Button variant="outline">
+            <LogInIcon className="mr-2" />
+            Fazer login ou criar conta
+          </Button>
+        </SignInButton>
       </div>
 
       {/* DIREITA */}
