1667a3fd5cb595cc42898b882130760a70fa815a
reggidev <reggidev@gmail.com>
2024-11-06 13:14:47 -0300

    feat: add login page

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,5 +1,5 @@
-const Subscription = () => {
-  return <h1>Subscription Page</h1>
+const SubscriptionPage = () => {
+  return <h1>SubscriptionPage Page</h1>
 }
 
-export default Subscription;
+export default SubscriptionPage
