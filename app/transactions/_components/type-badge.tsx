571bff928ee04ae702773e4bb058111a1cb048a0
reggidev <reggidev@gmail.com>
2024-11-08 17:58:54 -0300

    feat: add transaction type badge

---
+++ app/transactions/_components/type-badge.tsx
@@ -0,0 +1,35 @@
+import { Transaction, TransactionType } from "@prisma/client";
+import { CircleIcon } from "lucide-react";
+
+import { Badge } from "@/app/_components/ui/badge";
+
+interface TransactionTypeBadgeProps {
+  transaction: Transaction;
+}
+
+const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
+  if (transaction.type === TransactionType.DEPOSIT) {
+    return (
+      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+        <CircleIcon className="mr-1 fill-primary" size={10} />
+        Depósito
+      </Badge>
+    );
+  }
+  if (transaction.type === TransactionType.EXPENSE) {
+    return (
+      <Badge className="bg-danger/10 font-bold text-danger">
+        <CircleIcon className="mr-1 fill-danger" size={10} />
+        Despesa
+      </Badge>
+    );
+  }
+  return (
+    <Badge className="bg-white/10 font-bold text-white">
+      <CircleIcon className="mr-1 fill-white" size={10} />
+      Investimento
+    </Badge>
+  );
+};
+
+export default TransactionTypeBadge;
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,5 +1,25 @@
-const TransactionsPage = () => {
-  return <h1>Transactions page</h1>
-}
+import { ArrowDownUpIcon } from "lucide-react";
 
-export default TransactionsPage
+import { Button } from "../_components/ui/button";
+import { DataTable } from "../_components/ui/data-table";
+import { db } from "../_lib/prisma";
+import { TransactionsColumns } from "./_columns";
+
+const TransactionsPage = async () => {
+  const transactions = await db.transaction.findMany({});
+  return (
+    <div className="space-y-6 p-6">
+      {/* TÍTULO E BOTÃO */}
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowDownUpIcon />
+        </Button>
+      </div>
+      <DataTable columns={TransactionsColumns} data={transactions} />
+    </div>
+  );
+};
+
+export default TransactionsPage;
