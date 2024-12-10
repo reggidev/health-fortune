4adb4bd2850da54a3ee5ad39578b420862b83cbf
reggidev <reggidev@gmail.com>
2024-12-10 12:52:05 -0300

    fix: ensure user can scroll on transactions page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -4,6 +4,7 @@ import { redirect } from 'next/navigation'
 import AddTransactionButton from '../_components/add-transaction-button'
 import NavBar from '../_components/navbar'
 import { DataTable } from '../_components/ui/data-table'
+import { ScrollArea } from '../_components/ui/scroll-area'
 import { db } from '../_lib/prisma'
 import { TransactionsColumns } from './_columns'
 
@@ -24,16 +25,17 @@ const TransactionsPage = async () => {
   return (
     <>
       <NavBar />
-      <div className="space-y-6 overflow-hidden p-6">
-        {/* TÍTULO E BOTÃO */}
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton />
         </div>
-        <DataTable
-          columns={TransactionsColumns}
-          data={JSON.parse(JSON.stringify(transactions))}
-        />
+        <ScrollArea className="h-full">
+          <DataTable
+            columns={TransactionsColumns}
+            data={JSON.parse(JSON.stringify(transactions))}
+          />
+        </ScrollArea>
       </div>
     </>
   )
