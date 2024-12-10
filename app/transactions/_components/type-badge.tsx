4f9c14f4ef1a54ce3e383f3aeb273fb3e8122ddf
reggidev <reggidev@gmail.com>
2024-12-10 12:17:24 -0300

    fix: ensure colors are right when user hovered it

--- app/transactions/_components/type-badge.tsx
+++ app/transactions/_components/type-badge.tsx
@@ -10,7 +10,7 @@ interface TransactionTypeBadgeProps {
 const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
   if (transaction.type === TransactionType.DEPOSIT) {
     return (
-      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+      <Badge className="bg-muted font-bold text-primary hover:bg-primary/20">
         <CircleIcon className="mr-1 fill-primary" size={10} />
         Depósito
       </Badge>
@@ -18,14 +18,14 @@ const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
   }
   if (transaction.type === TransactionType.EXPENSE) {
     return (
-      <Badge className="bg-danger/10 font-bold text-danger">
+      <Badge className="bg-danger/10 font-bold text-danger hover:bg-danger/20">
         <CircleIcon className="mr-1 fill-danger" size={10} />
         Despesa
       </Badge>
     )
   }
   return (
-    <Badge className="bg-white/10 font-bold text-white">
+    <Badge className="bg-white/10 font-bold text-white hover:bg-white/20">
       <CircleIcon className="mr-1 fill-white" size={10} />
       Investimento
     </Badge>
